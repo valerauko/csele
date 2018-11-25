a60007cfeab4e6b367229b2e8047f38a544c73a7
Unknown <vale@valerauko.net>
2018-11-25 16:58:36 +0900

    Add hash-test

---
+++ test/csele/fixtures/hash.clj
@@ -0,0 +1,10 @@
+(ns csele.fixtures.hash)
+
+(def foo-sha3-512
+  "4bca2b137edc580fe50a88983ef860ebaca36c857b1f492839d6d7392452a63c82cbebc68e3b70a2a1480b4bb5d437a7cba6ecf9d89f9ff3ccd14cd6146ea7e7")
+
+(def foo-sha3-384
+  "665551928d13b7d84ee02734502b018d896a0fb87eed5adb4c87ba91bbd6489410e11b0fbcc06ed7d0ebad559e5d3bb5")
+
+(def foo-base64
+  "LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=")
---
+++ test/csele/hash_test.clj
@@ -0,0 +1,20 @@
+(ns csele.hash-test
+  (:require [clojure.test :refer :all]
+            [byte-streams :as bs]
+            [csele.fixtures.hash :as fix]
+            [csele.hash :refer :all])
+  (:import [java.io ByteArrayInputStream]))
+
+(deftest sha3-test
+  (testing "Generates 512 bit SHA3 hash by default"
+    (is (= (hash-hex "foo") fix/foo-sha3-512)))
+  (testing "Can generate other valid SHA3 bit strengths"
+    (is (= (hash-hex "foo" 384) fix/foo-sha3-384))))
+
+(deftest base64-test
+  (let [consumed-input (doto (-> "foo" .getBytes ByteArrayInputStream.)
+                             (bs/to-byte-array))]
+    (testing "Produces correct base64 hash of input"
+      (is (= (hash-base64 "foo") fix/foo-base64)))
+    (testing "Works with consumed streams too"
+      (is (= (hash-base64 consumed-input) fix/foo-base64)))))
