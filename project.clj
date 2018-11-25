3397c1dded3d618a8413b102646df8a9f6c28716
Unknown <vale@valerauko.net>
2018-11-25 16:54:46 +0900

    Add missing dependencies
    thpr

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [commons-codec/commons-codec "1.11"]
+                 [org.bouncycastle/bcprov-jdk15on "1.60"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.60"]
                  [byte-streams "0.2.4"]])
