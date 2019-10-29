fbd213525a10b6f4308b3c1f6fa8a86ea60703a1
Vale <valerauko@gmail.com>
2019-10-29 17:59:30 +0900

    Update deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://kitsune.social"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [commons-codec/commons-codec "1.11"]
-                 [org.bouncycastle/bcprov-jdk15on "1.60"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.60"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [commons-codec/commons-codec "1.13"]
+                 [org.bouncycastle/bcprov-jdk15on "1.64"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.64"]
                  [byte-streams "0.2.4"]]
   :profiles {:dev {:plugins [[lein-ancient "0.6.15"
                               :exclusions [org.clojure/clojure]]]}})
