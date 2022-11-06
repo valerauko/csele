de9908c583d3c0bc864b8d4899db4f6d87d8fe01
Vale <valerauko@gmail.com>
2022-11-06 23:20:07 +0900

    Test with string key

--- test/csele/fixtures/headers.clj
+++ test/csele/fixtures/headers.clj
@@ -9,7 +9,7 @@
    :body "foo"})
 
 (def dummy-signature
-  "keyId=\"hoge\",algorithm=\"rsa-sha256\",headers=\"(request-target) host date digest content-type\",signature=\"AhXPP5lUQJgcF6Om3A0K/UCnJ5AWYc+zvSZFuVhNHVHGVKSiFf3IDeKDv0i2agOLY/6LjxmbQBZ9Raqjq9sFFrhWe7mRuut5U0TdWYoElaY45iuXyMIlwERqHppLOvWRI+MubWlZCyMKKRxfxbJ+mCXXXNTKxg+5Fvc3ugvrD5I=\""                             )
+  "keyId=\"hoge\",algorithm=\"rsa-sha256\",headers=\"(request-target) host date digest content-type\",signature=\"AhXPP5lUQJgcF6Om3A0K/UCnJ5AWYc+zvSZFuVhNHVHGVKSiFf3IDeKDv0i2agOLY/6LjxmbQBZ9Raqjq9sFFrhWe7mRuut5U0TdWYoElaY45iuXyMIlwERqHppLOvWRI+MubWlZCyMKKRxfxbJ+mCXXXNTKxg+5Fvc3ugvrD5I=\"")
 
 (def signed-request
-  (assoc-in dummy-request [:headers :signature] dummy-signature))
+  (assoc-in dummy-request [:headers "signature"] dummy-signature))
