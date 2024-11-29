2f4d12b6878757ab43e1e80688fb3d3ff7d9c944
Vale <valerauko@gmail.com>
2024-11-29 22:37:16 +0900

    Update deps

--- project.clj
+++ project.clj
@@ -3,20 +3,23 @@
   :url "https://kitsune.social"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :dependencies [[commons-codec/commons-codec "1.15"]
-                 [org.bouncycastle/bcprov-jdk18on "1.76"]
-                 [org.bouncycastle/bcpkix-jdk18on "1.76"]]
+  :dependencies [[commons-codec/commons-codec "1.17.1"]
+                 [org.bouncycastle/bcprov-jdk18on "1.79"]
+                 [org.bouncycastle/bcpkix-jdk18on "1.79"]]
   :deploy-repositories {"clojars" {:url "https://repo.clojars.org/"
                                    :username :env/clojars_user
                                    :password :env/clojars_token}}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
+                                  [lambdaisland/kaocha "1.91.1392"]]
                    :global-vars {*warn-on-reflection* true
                                  *unchecked-math* :warn-on-boxed}
-                   :plugins [[lein-ancient "0.6.15"
+                   :plugins [[lein-ancient "0.7.0"
                               :exclusions [org.clojure/clojure]]]}
-             :clj1.9.0 {:dependencies
-                        [[org.clojure/clojure "1.9.0"]]}
-             :clj1.10.3 {:dependencies
-                         [[org.clojure/clojure "1.10.3"]]}
-             :clj1.11.1 {:dependencies
-                         [[org.clojure/clojure "1.11.1"]]}})
+             :clj1.9 {:dependencies
+                      [[org.clojure/clojure "1.9.0"]]}
+             :clj1.10 {:dependencies
+                       [[org.clojure/clojure "1.10.3"]]}
+             :clj1.11 {:dependencies
+                       [[org.clojure/clojure "1.11.4"]]}
+             :clj1.12 {:dependencies
+                       [[org.clojure/clojure "1.11.4"]]}})
