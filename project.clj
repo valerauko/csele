4e20957035f0ed50ae60878bc34ee5596f24cbc6
Vale <valerauko@gmail.com>
2024-11-29 22:44:10 +0900

    Release with updated deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject social.kitsune/csele "0.7.1"
+(defproject social.kitsune/csele "0.7.2"
   :description "A library to handle ActivityPub-related crypto needs."
   :url "https://kitsune.social"
   :license {:name "Eclipse Public License"
