5a598de7f163516a58e1af3e63e9cf9cee8cdaad
Vale <valerauko@gmail.com>
2026-07-02 21:23:28 +0900

    Update deps and actually test with kaocha

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://kitsune.social"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :dependencies [[commons-codec/commons-codec "1.17.1"]
-                 [org.bouncycastle/bcprov-jdk18on "1.79"]
-                 [org.bouncycastle/bcpkix-jdk18on "1.79"]]
+  :dependencies [[commons-codec/commons-codec "1.22.0"]
+                 [org.bouncycastle/bcprov-jdk18on "1.84"]
+                 [org.bouncycastle/bcpkix-jdk18on "1.84"]]
   :deploy-repositories {"clojars" {:url "https://repo.clojars.org/"
                                    :username :env/clojars_user
                                    :password :env/clojars_token}}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.5"]
                                   [lambdaisland/kaocha "1.91.1392"]]
                    :global-vars {*warn-on-reflection* true
                                  *unchecked-math* :warn-on-boxed}
@@ -22,4 +22,5 @@
              :clj1.11 {:dependencies
                        [[org.clojure/clojure "1.11.4"]]}
              :clj1.12 {:dependencies
-                       [[org.clojure/clojure "1.12.0"]]}})
+                       [[org.clojure/clojure "1.12.5"]]}}
+  :aliases {"test" ["run" "-m" "kaocha.runner"]})
